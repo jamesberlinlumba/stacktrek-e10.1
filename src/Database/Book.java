75c5df783fb685aca01ff347df2e6f390360948b
jamesberlinlumba <james.lumba@novare.com.hk>
2023-03-08 10:08:43 +0800

    removed unnecessary codes

--- src/Database/Book.java
+++ src/Database/Book.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Book {
-    private Map<Integer, String> books = new HashMap<Integer, String>();
+    private Map<Integer, String> books = new HashMap<>();
     private static Book instance;
     private Book() {}
     public static synchronized Book getInstance() {
@@ -15,7 +15,7 @@ public class Book {
         return instance;
     }
 
-    public Map viewBooks() {
+    public Map<Integer, String> viewBooks() {
         return books;
     }
     public void addBook(String bookName) {
@@ -25,14 +25,12 @@ public class Book {
     }
 
     public void removeBook(int bookId) {
-        if (books.containsKey(bookId)) {
-            books.remove(bookId);
-        }
+        books.remove(bookId);
     }
 
     public void removeBook(String bookName) {
         if (books.containsValue(bookName)) {
-            for (Map.Entry item : books.entrySet()) {
+            for (Map.Entry<Integer, String> item : books.entrySet()) {
                 if (item.getValue().equals(bookName)) {
                     books.remove(item.getKey());
                 }
